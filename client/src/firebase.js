f7656d7c498a4ae81512f8e5879dd24eba3ba773
Absaar <absaarali1548@gmail.com>
2023-12-04 14:13:17 +0530

    FireBase Authentication

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,14 +4,21 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "homelandestates-c4a1b.firebaseapp.com",
-  projectId: "homelandestates-c4a1b",
-  storageBucket: "homelandestates-c4a1b.appspot.com",
-  messagingSenderId: "907487074365",
-  appId: "1:907487074365:web:4369110c3d71fd9ba0f254"
+  authDomain: "homelandestates-49271.firebaseapp.com",
+  projectId: "homelandestates-49271",
+  storageBucket: "homelandestates-49271.appspot.com",
+  messagingSenderId: "785521061316",
+  appId: "1:785521061316:web:ab448a7f273fa7886f0891"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
